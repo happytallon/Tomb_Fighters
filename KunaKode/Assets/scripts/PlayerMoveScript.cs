b491fa7646c0dafafdd00b29a16cdcebf443eca7
happytallon <srmarcovich@hotmail.com>
2018-04-17 21:02:18 -0300

    Unity Update
    ...

---
+++ KunaKode/Assets/scripts/PlayerMoveScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{	
+	public Rigidbody2D rigidbody;
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		var x = Input.GetAxis("Horizontal");
+		rigidbody.velocity = new Vector2(x, 0) * 5;
+	}
+
+	void OnMouseDown()
+	{
+		
+	}
+}
